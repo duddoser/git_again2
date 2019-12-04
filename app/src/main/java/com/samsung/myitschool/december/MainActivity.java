e3c3ffe7c49d4587a37c5518b5fa7896a71ea8d5
Sylide <fekolkin.andrei@gmail.com>
2019-12-04 17:52:39 +0300

    Merge branch 'master' of C:\Users\fekol\Downloads\December with conflicts.

--- app/src/main/java/com/samsung/myitschool/december/MainActivity.java
+++ app/src/main/java/com/samsung/myitschool/december/MainActivity.java
@@ -3,22 +3,14 @@ package com.samsung.myitschool.december;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Two plus two is four = quick math
-        System.out.print("DASIP WAS HERE!");
-
-    }
-
-
-    @Override
-    public void onClick(View view) {
+        //Как дела босс?
 
     }
 }
